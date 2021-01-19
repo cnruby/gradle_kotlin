90257038d2f3feeb5a03f7fd16ead64dfac37291
gudao.luo@gmail.com <gudao.luo@gmail.com>
2021-01-19 17:43:59 +0100

    Unit 222: Hello JSONObject!

--- src/main/kotlin/de/iotoi/HelloRestController.kt
+++ src/main/kotlin/de/iotoi/HelloRestController.kt
@@ -1,12 +1,39 @@
 package de.iotoi
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.json.JSONObject
+import java.util.HashMap
 
 @RestController
 class HelloRestController(val helloService: HelloService) {
-    @GetMapping("/api")
-    fun helloKotlin(): String {
-        return helloService.getHello()
+    @GetMapping("/api/str")
+    fun helloString(): String {
+        return helloService.getStringHello()
+    }
+
+    @GetMapping(path= ["/api/resp"], produces= [MediaType.APPLICATION_JSON_VALUE])
+    fun helloResponseEntity(): ResponseEntity<String> {
+        val jsonResp = helloService.getResponseEntityHello()
+        return ResponseEntity(jsonResp.toString(), HttpStatus.OK)
+    }
+
+    @GetMapping("/api/map")
+    fun helloMap(): ResponseEntity<String> {
+        val jsonMap = JSONObject()
+
+        val map: MutableMap<String, String> = helloService.getMapHello()
+        map.entries.stream().forEach {
+            (key, value) -> jsonMap.put( "$key", "$value" )
+        }
+
+//        map.forEach {
+//            (key, value) -> jsonMap.put( "$key", "$value" )
+//        }
+
+        return ResponseEntity(jsonMap.toString(), HttpStatus.OK)
     }
 }
--- src/main/kotlin/de/iotoi/HelloService.kt
+++ src/main/kotlin/de/iotoi/HelloService.kt
@@ -2,13 +2,29 @@ package de.iotoi
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import org.json.JSONObject
+import java.util.HashMap
 
 //@Service("ms")
 @Service()
 class HelloService {
     @Value("\${web.app.name}") val webAppName: String? = null
 
-    fun getHello(): String {
-        return "$webAppName!\n"
+    fun getStringHello(): String {
+        val jsonObj = JSONObject()
+        jsonObj.put("String", "$webAppName")
+        return jsonObj.toString()
+    }
+
+    fun getJSONObjectHello(): JSONObject {
+        val jsonEntity = JSONObject()
+        jsonEntity.put("ResponseEntity", "$webAppName")
+        return jsonEntity
+    }
+
+    fun getMapHello(): MutableMap<String, String> {
+        val map: MutableMap<String, String> = HashMap()
+        map["Map"] = "$webAppName"
+        return map
     }
 }
